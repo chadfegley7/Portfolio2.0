b0a5535787480225faa9f882a3b43eeb38e82b66
Chad Fegley <chadfegley7@gmail.com>
2017-01-06 12:04:21 -0800

    Flickr

--- client/controllers/imageCtrl.js
+++ client/controllers/imageCtrl.js
@@ -1,6 +1,6 @@
 App.controller('imageCtrl', function($scope, $http){
 
-  $http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=753f95ec28dd07aff0c749b4aa00a812&photoset_id=72157672246593266&user_id=134646885%40N03&format=json&nojsoncallback=1&auth_token=72157675058702413-a151d45a4c09b6f7&api_sig=b3fd6ec42e077f2eb91e288ce94eadd0").then(function(response){
+  $http.get("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=0a9e130425e50f9173010ea8a0bd6bf5&photoset_id=72157672246593266&user_id=134646885%40N03&per_page=15&format=json&nojsoncallback=1&auth_token=72157675018562974-76cf823bae4a7016&api_sig=f2a1a78b3c2be9b1326141ec33f2a6b4").then(function(response){
 
     $scope.picture = response.data.photoset.photo;
     console.log($scope.picture);
